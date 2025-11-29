41c9369633094d05593f20e13a6bc61f445f3899
dfouadd <dina.serag@gmail.com>
2025-11-29 15:29:48 +0200

    Products Component Style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { words } from "../src/words";
+import Products from "./components/products/Products";
+import Filter from "./components/filter/Filter";
+import data from "./data.json";
+import words from "./words";
 
 function App() {
+  const [products, setProducts] = useState(data);
+  console.log(products);
   return (
     <div className="layout">
       <Header />
-      <main>{words.content}</main>
+      <main>
+        <div className="wrapper">
+          <Products products={products} />
+          <Filter />
+        </div>
+      </main>
       <Footer />
     </div>
   );
---
+++ client/src/components/filter/Filter.jsx
@@ -0,0 +1,5 @@
+const Filter = () => {
+  return <div className="filter-wrapper">Filter</div>;
+};
+
+export default Filter;
